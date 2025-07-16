4ecf2188d14b3dcf7a134fb7948037fb65bc2801
Ubuntu <ubuntu@ip-172-31-55-118.us-west-2.compute.internal>
2025-07-16 10:36:07 +0000

    add env

--- development.sh
+++ development.sh
@@ -9,5 +9,10 @@ ln -s build/lib.linux-x86_64-cpython-310/deep_ep_cpp.cpython-310-x86_64-linux-gn
 # NOTES: you may modify the `init_dist` function in `tests/utils.py`
 # according to your own cluster settings, and launch into multiple nodes 
 python3 tests/test_intranode.py
-python3 tests/test_internode.py
 python3 tests/test_low_latency.py
+
+export MASTER_ADDR=172.31.55.118  # 主节点IP
+export MASTER_PORT=29500       # 主节点端口
+export WORLD_SIZE=2            # 节点总数
+export RANK=0  # 0,1
+python3 tests/test_internode.py

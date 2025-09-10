ee5bd170991b3bd8f50c581c6d0962cba6b64fa6
Chenggang Zhao <chenggangz@deepseek.com>
2025-09-10 10:09:54 +0800

    Update version number

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ if __name__ == '__main__':
 
     setuptools.setup(
         name='deep_ep',
-        version='1.1.0' + revision,
+        version='1.2.0' + revision,
         packages=setuptools.find_packages(
             include=['deep_ep']
         ),
